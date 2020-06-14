b1e4a3e608760875b1d2fe4009144722db2ba92d
felixlonald2 <55523511+felixlonald2@users.noreply.github.com>
2020-06-15 05:34:01 +0700

    hapus hapus yang tidak penting

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
-      port = process.env.port || 3000,
-    //   PORT = 3000,
+      port = process.env.port || 3000,=
       fetch = require('node-fetch'),
       db = require('./database');
 const request= require('request');
@@ -28,40 +27,6 @@ const storage = multer.diskStorage({
 const uploads = multer({
     storage: storage
 });
-
-
-app.get('/', async (req, res) => {
-    console.log("CHECK USER....")
-    let query= await db.executeQuery(`
-        select * from users
-    `);
-    console.log("MENAMPILKAN USER....")
-    return res.status(200).json({
-        status: 200,
-        user: query.rows
-    });
-});
-
-//felix
-app.get('/api/getNews', async (req, res) => {
-    let query= await db.executeQuery(`
-        select * from berita
-    `);
-    var berita = query.rows;
-    var tampung = [];
-
-    for (let i = 0; i < berita.length; i++) {
-        const beritabaru = {
-            Judul_berita : berita[i].judul,
-            Deskripsi_berita : berita[i].deskripsi,
-            Publish : berita[i].tanggal
-        }
-        tampung.push(beritabaru);
-    }
-
-    res.status(200).send(tampung)
-});
-
 
 app.listen(process.env.PORT,function(){
     console.log("Listening to port "+process.env.PORT);
